dc421fa5658869f94d54307a82e2eb4415d13418
Doublepicebs <nananny2005@gmail.com>
2020-01-12 20:56:34 +0200

    social update

---
+++ main.js
@@ -0,0 +1,5 @@
+var flexme = document.getElementById("flexme");
+var nav = document.getElementById("nav");
+nav.style.display = "none; ! important";
+console.log(flexme.style.width);
+console.log(nav.style.display);
